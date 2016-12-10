a3c41cdf99163de630fd830d4682e1ddbfc65682
fuchuan <332553703@qq.com>
2016-12-11 04:24:58 +0800

    移除jquery 依赖

--- src/js/jquery.colorPicker.js
+++ src/js/jquery.colorPicker.js
@@ -1,109 +1,152 @@
 /**
  * 颜色选择器
+ * 1.0.3 去jquery
+ * 1.0.2 兼容rgba,修复精度丢失
  * 1.0.0 正式发布
  */
 ;
-(function ($) {
-
+(function () {
+	Util = {
+		createElementByHtml: function (html) {
+			var temp = document.createElement('div');
+			temp.innerHTML = html;
+			return temp.children.length == 1 ? temp.children[0] : temp.childNodes;
+		},
+		getElementOffset: function (elem) {
+			return elem.getClientRects()[0];
+		},
+		extend: function () {
+			var re = arguments[0];
+			for (var i = 1; i < arguments.length; i++) {
+				var obj = arguments[i];
+				for (var key in obj) {
+					re[key] = obj[key];
+				}
+			}
+			return re;
+		},
+		getRGBAforStr(str) {
+			var match = str.match(/[0-9\.]+/g) || [];
+			return rgb = {
+				r: Number.parseInt(match[0]),
+				g: Number.parseInt(match[1]),
+				b: Number.parseInt(match[2]),
+				a: Number.parseFloat(match[3])
+			};
+		}
+	};
 	window.colorPicker = function (option) {
 		return new colorPicker.fn.init(option);
 	}
 
 	colorPicker.fn = colorPicker.prototype = {
 		'init': function (option) {
-			var config = $.extend(colorPicker.config, option); //合并默认参数
+			var config = Util.extend(colorPicker.config, option); //合并默认参数
 			this.config = config;
 
 			var picker = this;
-			var $colorPicker = $(colorPicker.htmlTemplates);
+			var $colorPicker = Util.createElementByHtml(colorPicker.htmlTemplates);
 			picker.$colorPicker = $colorPicker;
-			$colorPicker.bind("click", function () {
-				return false
+			$colorPicker.addEventListener("click", function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				return false;
 			});
 
 			//调节饱和度和亮度
-			var $sv = $colorPicker.find(".sv");
+			var $sv = $colorPicker.querySelector(".sv");
 
 			function onsvmove(event) {
-				var top = event.pageY - $sv.find(".value").offset().top;
+				var elemOffset = Util.getElementOffset($sv.querySelector(".value"));
+				var top = event.pageY - elemOffset.top;
 				if (top < -5) {
 					top = -5;
 				}
-				if (top > $sv.find(".value").height() - 5) {
-					top = $sv.find(".value").height() - 5;
+				if (top > elemOffset.height - 5) {
+					top = elemOffset.height - 5;
 				}
-				$sv.find(".point").css("top", top + 'px');
+				$sv.querySelector(".point").style.top = top + 'px';
 
-				var left = event.pageX - $sv.find(".value").offset().left;
+				var left = event.pageX - elemOffset.left;
 				if (left < -5) {
 					left = -5;
 				}
-				if (left > $sv.find(".value").width() - 5) {
-					left = $sv.find(".value").width() - 5;
+				if (left > elemOffset.width - 5) {
+					left = elemOffset.width - 5;
 				}
-				$sv.find(".point").css("left", left + 'px');
+				$sv.querySelector(".point").style.left = left + 'px';
 
 				picker.refresh('hsv');
 			}
-			$sv.find(".value,.point").bind("mousedown", function (event) {
+			$sv.querySelector(".value,.point").addEventListener("mousedown", function (event) {
+				function mouseup() {
+					document.removeEventListener("mousemove", onsvmove);
+					document.removeEventListener("mouseup", mouseup);
+				}
 				onsvmove(event);
-				$(document).bind("mousemove", onsvmove);
-				$(document).bind("mouseup", function () {
-					$(document).unbind("mousemove", onsvmove);
-				});
+				document.addEventListener("mousemove", onsvmove);
+				document.addEventListener("mouseup", mouseup);
 			});
 
 			//色相调节区
-			var $h = $colorPicker.find(".h");
+			var $h = $colorPicker.querySelector(".h");
 
 			function onhmove(event) {
-				var top = event.pageY - $h.find(".value").offset().top;
+				var offset = Util.getElementOffset($h.querySelector(".value"));
+				var top = event.pageY - offset.top;
 				if (top < 0) {
 					top = 0;
 				}
-				if (top > $h.find(".value").height() - 1) {
-					top = $h.find(".value").height() - 1;
+				if (top > offset.height - 1) {
+					top = offset.height - 1;
 				}
-				$h.find(".point").css("top", top);
+				$h.querySelector(".point").style.top = top + 'px';
 				picker.refresh('hsv');
 			}
-			$h.find(".value,.point").bind("mousedown", function (event) {
+			$h.querySelector(".value,.point").addEventListener("mousedown", function (event) {
+				function mouseup() {
+					document.removeEventListener("mousemove", onhmove);
+					document.removeEventListener("mouseup", mouseup);
+				}
 				onhmove(event);
-				$(document).bind("mousemove", onhmove);
-				$(document).bind("mouseup", function () {
-					$(document).unbind("mousemove", onhmove);
-				});
+				document.addEventListener("mousemove", onhmove);
+				document.addEventListener("mouseup", mouseup);
 			});
 
 
 			//透明度调节区
-			var $alpha = $colorPicker.find(".alpha");
+			var $alpha = $colorPicker.querySelector(".alpha");
 
 			function onalphamove(event) {
-				var top = event.pageY - $alpha.find(".value").offset().top;
+				var offset = Util.getElementOffset($alpha.querySelector(".value"));
+				var top = event.pageY - offset.top;
 				if (top < 0) {
 					top = 0;
 				}
-				if (top > $alpha.find(".value").height() - 1) {
-					top = $alpha.find(".value").height() - 1;
+				if (top > offset.height - 1) {
+					top = offset.height - 1;
 				}
-				$alpha.find(".point").css("top", top);
+				$alpha.querySelector(".point").style.top = top + 'px';
 				picker.refresh('alpha');
 			}
-			$alpha.find(".value,.point").bind("mousedown", function (event) {
+			$alpha.querySelector(".value,.point").addEventListener("mousedown", function (event) {
+				function mouseup() {
+					document.removeEventListener("mousemove", onalphamove);
+					document.removeEventListener("mouseup", mouseup);
+				}
 				onalphamove(event);
-				$(document).bind("mousemove", onalphamove);
-				$(document).bind("mouseup", function () {
-					$(document).unbind("mousemove", onalphamove);
-				});
+				document.addEventListener("mousemove", onalphamove);
+				document.addEventListener("mouseup", mouseup);
 			});
 
 			//调节值显示区			
-			$colorPicker.children(".value").find("input").bind("change", function () {
-				picker.refresh(this.name);
-			});
+			Array.prototype.map.call($colorPicker.querySelectorAll(".values input"), function (elem) {
+				elem.addEventListener("change", function () {
+					picker.refresh(this.name);
+				});
+			})
 			//添加到页面
-			$("body").append($colorPicker);
+			document.body.appendChild($colorPicker);
 
 			//显示
 			if (config.show) {
@@ -111,76 +154,63 @@
 			}
 			return picker;
 		},
-		/**
-		 * 刷新页面
-		 * type 发生改变的位置,
-		 */
-		'refresh': function (type) {
+		'getValues': function (type) {
 			type = type || "hsv";
 			var picker = this;
 			var h, s, v;
 			var rgb;
 			var hex;
-			var alpha = (picker.$colorPicker.find(".alpha").find(".point").offset().top - picker.$colorPicker.find(".alpha").offset().top) / 180;
-
+			var alpha = (picker.$colorPicker.querySelector(".alpha").querySelector(".point").offsetTop - picker.$colorPicker.querySelector(".alpha").offsetTop) / 180;
 			switch (type) {
-				case "hsv":
-					h = (picker.$colorPicker.find(".h").find(".point").offset().top - picker.$colorPicker.find(".h").offset().top) * 2;
-					s = (picker.$colorPicker.find(".sv").find(".point").offset().left + 5 - picker.$colorPicker.find(".sv").offset().left) / 180;
-					v = 1 - (picker.$colorPicker.find(".sv").find(".point").offset().top + 5 - picker.$colorPicker.find(".sv").offset().top) / 180;
-					break;
-				case "h":
-					h = Number.parseFloat(picker.$colorPicker.children(".value").find("input[name='h']").val());
-					h = isNaN(h) ? picker.$colorPicker.children(".value").find("input[name='h']").data("value") : h;
-					s = (picker.$colorPicker.find(".sv").find(".point").offset().left - picker.$colorPicker.find(".sv").offset().left) / 180;
-					v = 1 - (picker.$colorPicker.find(".sv").find(".point").offset().top - picker.$colorPicker.find(".sv").offset().top) / 180;
-					break;
-				case "s":
-					h = (picker.$colorPicker.find(".h").find(".point").offset().top - picker.$colorPicker.find(".h").offset().top) * 2;
-					s = Number.parseFloat(picker.$colorPicker.children(".value").find("input[name='s']").val());
-					s = isNaN(s) ? picker.$colorPicker.children(".value").find("input[name='s']").data("value") : s;
-					v = 1 - (picker.$colorPicker.find(".sv").find(".point").offset().top - picker.$colorPicker.find(".sv").offset().top) / 180;
-					break;
-				case "v":
-					h = (picker.$colorPicker.find(".h").find(".point").offset().top - picker.$colorPicker.find(".h").offset().top) * 2;
-					s = (picker.$colorPicker.find(".sv").find(".point").offset().left - picker.$colorPicker.find(".sv").offset().left) / 180;
-					v = Number.parseFloat(picker.$colorPicker.children(".value").find("input[name='v']").val());
-					v = isNaN(v) ? picker.$colorPicker.children(".value").find("input[name='v']").data("value") : v;
-					break;
+				// case "h":
+				// 	h = Number.parseFloat(picker.$colorPicker.querySelector(".values").querySelector("input[name='h']").value);
+				// 	h = isNaN(h) ? picker.$colorPicker.querySelector(".values").querySelector("input[name='h']").getAttribute("value") : h;
+				// 	s = Util.getElementOffset(picker.$colorPicker.querySelector(".sv").querySelector(".point").left - Util.getElementOffset(picker.$colorPicker.querySelector(".sv")).left) / 180;
+				// 	v = 1 - Util.getElementOffset(picker.$colorPicker.querySelector(".sv").querySelector(".point").top - Util.getElementOffset(picker.$colorPicker.querySelector(".sv")).top) / 180;
+				// 	break;
+				// case "s":
+				// 	h = (picker.$colorPicker.querySelector(".h").querySelector(".point").offsetTop - picker.$colorPicker.querySelector(".h").offsetTop) * 2;
+				// 	s = Number.parseFloat(picker.$colorPicker.querySelector(".values").querySelector("input[name='s']").value);
+				// 	s = isNaN(s) ? picker.$colorPicker.querySelector(".values").querySelector("input[name='s']").getAttribute("value") : s;
+				// 	v = 1 - Util.getElementOffset(picker.$colorPicker.querySelector(".sv").querySelector(".point").top - picker.$colorPicker.querySelector(".sv").offsetTop) / 180;
+				// 	break;
+				// case "v":
+				// 	h = (picker.$colorPicker.querySelector(".h").querySelector(".point").offsetTop - picker.$colorPicker.querySelector(".h").offsetTop) * 2;
+				// 	s = Util.getElementOffset(picker.$colorPicker.querySelector(".sv").querySelector(".point").left - Util.getElementOffset(picker.$colorPicker.querySelector(".sv")).left) / 180;
+				// 	v = Number.parseFloat(picker.$colorPicker.querySelector(".values").querySelector("input[name='v']").value);
+				// 	v = isNaN(v) ? picker.$colorPicker.querySelector(".values").querySelector("input[name='v']").getAttribute("value") : v;
+				// 	break;
 				case "rgb":
-					var match = picker.$colorPicker.children(".value").find("input[name='rgb']").val().match(/[0-9]+/g) || [];
-					console.log(match);
-					rgb = {
-						r: Number.parseInt(match[0]),
-						g: Number.parseInt(match[1]),
-						b: Number.parseInt(match[2]),
-					};
+					var rgb = Util.getRGBAforStr(picker.$colorPicker.querySelector(".values").querySelector("input[name='rgb']").value);
+
 					if (rgb.r > 255 || rgb.r < 0 || rgb.g > 255 || rgb.g < 0 || rgb.b > 255 || rgb.b < 0) {
-						rgb = picker.$colorPicker.children(".value").find("input[name='rgb']").data("value");
+						rgb = picker.$colorPicker.querySelector(".values").querySelector("input[name='rgb']").getAttribute("value");
 					}
 					var hsv = colorPicker.rgb2hsv(rgb);
 					h = hsv.h;
 					s = hsv.s;
 					v = hsv.v;
+					alpha = rgb.a;
 					break;
-				case "hex":
-					var hex = picker.$colorPicker.children(".value").find("input[name='hex']").val().match(/[a-f0-9]{6}/i);
-					if (!hex) {
-						picker.$colorPicker.children(".value").find("input[name='hex']").val(picker.$colorPicker.children(".value").find("input[name='hex']").data('value'));
-						return;
-					}
-					hex = hex[0]
-					console.log(hex);
-					var rgb = colorPicker.hex2rgb(hex);
-					var hsv = colorPicker.rgb2hsv(rgb);
-					h = hsv.h;
-					s = hsv.s;
-					v = hsv.v;
-					break;
+				// case "hex":
+				// 	var hex = picker.$colorPicker.querySelector(".values").querySelector("input[name='hex']").value.match(/[a-f0-9]{6}/i);
+				// 	if (!hex) {
+				// 		picker.$colorPicker.querySelector(".values").querySelector("input[name='hex']").value = picker.$colorPicker.querySelector(".values").querySelector("input[name='hex']").getAttribute('data-value');
+				// 		return;
+				// 	}
+				// 	hex = hex[0]
+				// 	console.log(hex);
+				// 	var rgb = colorPicker.hex2rgb(hex);
+				// 	var hsv = colorPicker.rgb2hsv(rgb);
+				// 	h = hsv.h;
+				// 	s = hsv.s;
+				// 	v = hsv.v;
+				// 	break;
+				case 'hsv':
 				default:
-					h = (picker.$colorPicker.find(".h").find(".point").offset().top - picker.$colorPicker.find(".h").offset().top) * 2;
-					s = (picker.$colorPicker.find(".sv").find(".point").offset().left + 5 - picker.$colorPicker.find(".sv").offset().left) / 180;
-					v = 1 - (picker.$colorPicker.find(".sv").find(".point").offset().top + 5 - picker.$colorPicker.find(".sv").offset().top) / 180;
+					h = (Util.getElementOffset(picker.$colorPicker.querySelector(".h").querySelector(".point")).top - Util.getElementOffset(picker.$colorPicker.querySelector(".h")).top) * 2;
+					s = (Util.getElementOffset(picker.$colorPicker.querySelector(".sv").querySelector(".point")).left + 5 - Util.getElementOffset(picker.$colorPicker.querySelector(".sv")).left) / 180;
+					v = 1 - (Util.getElementOffset(picker.$colorPicker.querySelector(".sv").querySelector(".point")).top + 5 - Util.getElementOffset(picker.$colorPicker.querySelector(".sv")).top) / 180;
 					break;
 			}
 
@@ -193,56 +223,70 @@
 			h = h < 0 ? 0 : h;
 			s = s < 0 ? 0 : s;
 			v = v < 0 ? 0 : v;
+			alpha = alpha < 0 ? 0 : alpha;
 
 			rgb = rgb ? rgb : colorPicker.hsv2rgb({ h: h, s: s, v: v });
 			hex = hex ? hex : colorPicker.rgb2hex(rgb);
-			rgbStr = '';
-			if (alpha >= 0 && alpha < 1) {
-				rgbStr = "rgba(" + rgb.r + "," + rgb.g + "," + rgb.b + "," + alpha.toFixed(1) + ")";
-			} else {
-				rgbStr = "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b + ")";
-			}
-
+			rgbStr = "rgba(" + rgb.r + "," + rgb.g + "," + rgb.b + "," + alpha.toFixed(1) + ")";
+			return {
+				h: h,
+				s: s,
+				v: v,
+				alpha: alpha,
+				rgb: rgb,
+				hex: hex,
+				rgbStr: rgbStr,
+			};
+		},
+		/**
+		 * 刷新页面
+		 * type 发生改变的位置,
+		 */
+		'refresh': function (type, values) {
+			values = values || this.getValues(type);
+			var picker = this;
 			//设置显示区域
-			picker.$colorPicker.find(".h").find(".point").css("top", (h == 360 ? 358 : h) / 2);
-			picker.$colorPicker.find(".sv").css("background-color", "rgb(" + colorPicker.hsv2rgb({ h: h, s: 1, v: 1 }).rgb + ")");
-			picker.$colorPicker.find(".sv").find(".point").css({
-				// top: (1 - v >= 1 ? 1 : 1 - v) * 180 - 5,
-				// left: (s == 1 ? 1 : s) * 180 - 5,
-				top: (1 - v >= 1 ? 1 : 1 - v) * 180 - 5,
-				left: s * 180 - 5,
-			});
-			picker.$colorPicker.children(".value").find("input[name='h']").val(Math.floor(h)).data('value', h);
-			picker.$colorPicker.children(".value").find("input[name='s']").val(s.toFixed(2)).data('value', s);
-			picker.$colorPicker.children(".value").find("input[name='v']").val(v.toFixed(2)).data('value', v);
-			picker.$colorPicker.children(".value").find("input[name='rgb']").val(rgbStr).data('value', rgbStr);
-
-
-			picker.$colorPicker.children(".value").find("input[name='hex']").val("#" + hex).data('value', hex);
-
+			picker.$colorPicker.querySelector(".h").querySelector(".point").style.top = (values.h == 360 ? 358 : values.h) / 2;
+			picker.$colorPicker.querySelector(".sv").style.backgroundColor = "rgb(" + colorPicker.hsv2rgb({ h: values.h, s: 1, v: 1 }).rgb + ")";
+			picker.$colorPicker.querySelector(".sv").querySelector(".point").style.top = (1 - values.v >= 1 ? 1 : 1 - values.v) * 180 - 5 + 'px';
+			picker.$colorPicker.querySelector(".sv").querySelector(".point").style.left = values.s * 180 - 5 + 'px';
+			// picker.$colorPicker.querySelector(".sv").querySelector(".point").css({
+			// 	top: (1 - v >= 1 ? 1 : 1 - v) * 180 - 5,
+			// 	left: (s == 1 ? 1 : s) * 180 - 5,
+			// 	top: (1 - v >= 1 ? 1 : 1 - v) * 180 - 5,
+			// 	left: s * 180 - 5,
+			// });
+			// picker.$colorPicker.querySelector(".values").querySelector("input[name='h']").value = Math.floor(h);
+			// picker.$colorPicker.querySelector(".values").querySelector("input[name='h']").setAttribute("data-value", h);
+			// picker.$colorPicker.querySelector(".values").querySelector("input[name='s']").value = s.toFixed(2);
+			// picker.$colorPicker.querySelector(".values").querySelector("input[name='s']").setAttribute("data-value", s);
+			// picker.$colorPicker.querySelector(".values").querySelector("input[name='v']").value = v.toFixed(2);
+			// picker.$colorPicker.querySelector(".values").querySelector("input[name='v']").setAttribute("data-value", v);
+			picker.$colorPicker.querySelector(".values").querySelector("input[name='rgb']").value = values.rgbStr;
+			picker.$colorPicker.querySelector(".values").querySelector("input[name='rgb']").setAttribute("data-value", values.rgbStr);
+			// picker.$colorPicker.querySelector(".values").querySelector("input[name='hex']").value = "#" + hex;
+			// picker.$colorPicker.querySelector(".values").querySelector("input[name='hex']").setAttribute("data-value", hex);
 
 			//回调
 			if (picker.config.onChange) {
-				picker.config.onChange({
-					rgb: rgbStr,
-					hex: "#" + hex,
-				});
+				//为了不catch异常
+				setTimeout(function () {
+					picker.config.onChange({
+						rgb: values.rgbStr,
+						hex: "#" + values.hex,
+					});
+				}, 100);
 			}
 		},
 		'show': function () {
-			//隐藏其它选择器
-
-			$(".colorpicker").hide(300);
 			if (this.config.dom) {
-				this.$colorPicker.css({
-					'top': $(this.config.dom).offset().top + $(this.config.dom).height() + 10,
-					'left': $(this.config.dom).offset().left,
-				});
+				this.$colorPicker.style.top = this.config.dom.offsetTop + this.config.dom.offsetHeight + 10 + 'px';
+				this.$colorPicker.style.left = this.config.dom.offsetLeft + 'px';
 			}
-			this.$colorPicker.show(300);
+			this.$colorPicker.style.display = 'block';
 		},
 		'hide': function () {
-			this.$colorPicker.hide(300);
+			this.$colorPicker.style.display = 'none';
 		},
 	};
 	colorPicker.fn.init.prototype = colorPicker.fn;
@@ -253,7 +297,7 @@
 		+ "<div class='sv'>" + "<div class='value'></div>" + "<div class='point'></div>" + "</div>"
 		+ "<div class='h'>" + "<div class='value'></div>" + "<div class='point'></div>" + "</div>"
 		+ "<div class='alpha'>" + "<div class='value'></div>" + "<div class='point'></div>" + "</div>"
-		+ "<div class='value'>"
+		+ "<div class='values'>"
 		+ "<label>RGB:</label><input name='rgb'></input><br />"
 		// + "<label>HEX:</label><input name='hex'></input><br />"
 		// + "<label>H:</label><input name='h' ></input><br />"
@@ -346,33 +390,4 @@
 			b: parseInt((hex[4] || hex[2]) + (hex[5] || hex[2]), 16) / 255
 		};
 	};
-
-
-	//封装到Jquery 
-	$.fn.extend({
-		colorPicker: function (option) {
-			$(this).each(function (index) {
-				if (index != 0) return; //只显示一个
-				var elem = this;
-				var Picker;
-				if (elem.colorPicker) {
-					Picker = elem.colorPicker;
-				} else {
-					//初始化
-					option.dom = elem;
-					Picker = new colorPicker(option);
-					$(elem).bind("click", function () {
-						Picker.show();
-						return false;
-					});
-					$(document).bind("click", function () {
-						// Picker.hide();
-					});
-					elem.colorPicker = Picker;
-				}
-			});
-			return this;
-		},
-	});
-
-})($);
+})();
